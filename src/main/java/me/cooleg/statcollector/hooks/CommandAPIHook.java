79e34408413150e37ecf54783486126bb969b0c4
TrollsterCooleg <git@cooleg.me>
2023-09-15 19:45:51 -0500

    Add "/getfromobjective" command to simplify setting a stat for everyone using the value from an objective.

--- src/main/java/me/cooleg/statcollector/hooks/CommandAPIHook.java
+++ src/main/java/me/cooleg/statcollector/hooks/CommandAPIHook.java
@@ -3,9 +3,12 @@ import dev.jorel.commandapi.CommandAPICommand;
 import dev.jorel.commandapi.CommandPermission;
 import dev.jorel.commandapi.arguments.EntitySelectorArgument;
 import dev.jorel.commandapi.arguments.GreedyStringArgument;
+import dev.jorel.commandapi.arguments.ObjectiveArgument;
 import me.cooleg.statcollector.statisticsmanagement.PlayerStatistics;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
+import org.bukkit.scoreboard.Objective;
 
 public class CommandAPIHook {
 
@@ -16,10 +19,24 @@ public class CommandAPIHook {
                 .withPermission(CommandPermission.OP)
                 .executes(((commandSender, commandArguments) -> {
                     Player target = (Player) commandArguments.get("target");
-                    String msg = (String) commandArguments.get("statistic");
-                    if (target == null || msg == null) {commandSender.sendMessage(ChatColor.RED + "Missing proper arguments!"); return;}
+                    String stat = (String) commandArguments.get("statistic");
+                    if (target == null || stat == null) {commandSender.sendMessage(ChatColor.RED + "Missing proper arguments!"); return;}
 
-                    PlayerStatistics.getPlayer(target.getUniqueId()).addStatistic(msg, 1);
+                    PlayerStatistics.getPlayer(target.getUniqueId()).addStatistic(stat, 1);
+                })).register();
+
+        new CommandAPICommand("getfromobjective")
+                .withArguments(new ObjectiveArgument("objective"))
+                .withArguments(new GreedyStringArgument("statistic"))
+                .withPermission(CommandPermission.OP)
+                .executes(((commandSender, commandArguments) -> {
+                    Objective objective = (Objective) commandArguments.get("objective");
+                    String stat = (String) commandArguments.get("statistic");
+
+                    for (Player p : Bukkit.getOnlinePlayers()) {
+                        int score = objective.getScore(p.getName()).getScore();
+                        PlayerStatistics.getPlayer(p.getUniqueId()).setStatistic(stat, score);
+                    }
                 })).register();
     }
 
